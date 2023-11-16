a51e03d9b25841b3a362b8fbea8987c8d39f67e4
LautaroGVega <141835855+LautaroGVega@users.noreply.github.com>
2023-11-16 12:33:05 -0300

    Update UserService.java

--- grpcDemo/src/main/java/user/UserService.java
+++ grpcDemo/src/main/java/user/UserService.java
@@ -9,31 +9,26 @@ import io.grpc.stub.StreamObserver;
 
 public class UserService extends userImplBase{
 
-	@Override
+		@Override
 	public void login(LoginRequest request, StreamObserver<APIResponse> responseObserver) {
-		
-		System.out.println("Inside login");
-		
+		System.out.println("Inicio de sesión");
+	
 		String username = request.getUsername();
 		String password = request.getPassword();
-		
+	
 		APIResponse.Builder response = APIResponse.newBuilder();
-		if(username.equals(password)) {
-			
-			 // return success message
-			
-			response.setResponseCode(0).setResponsemessage("SUCCESS");
-			
+		if (username.equals(password)) {
+			// return success message
+			response.setResponseCode(0).setResponsemessage("Inicio aprobado para el usuario: " + username);
+			System.out.println("Usuario '" + username + "' ha iniciado sesión."); // Esta línea imprime el nombre del usuario
+		} else {
+			response.setResponseCode(100).setResponsemessage("Contraseña no válida");
 		}
-		else {
-			response.setResponseCode(100).setResponsemessage("INVALID PASSWORD");
-		}
-		
-		
-		
+	
 		responseObserver.onNext(response.build());
 		responseObserver.onCompleted();
 	}
+
 
 	@Override
 	public void logout(Empty request, StreamObserver<APIResponse> responseObserver) {
