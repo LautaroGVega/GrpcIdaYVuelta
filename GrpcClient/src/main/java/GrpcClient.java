b5ecbd65d1e4e463846fd841c247366728a3c02f
LautaroGVega <141835855+LautaroGVega@users.noreply.github.com>
2023-11-16 09:38:35 -0300

    Update GrpcClient.java

--- GrpcClient/src/main/java/GrpcClient.java
+++ GrpcClient/src/main/java/GrpcClient.java
@@ -6,23 +6,33 @@ import com.yrrhelp.grpc.userGrpc.userBlockingStub;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import java.util.Scanner;
+
 public class GrpcClient {
 
-	public static void main(String[] args) {
-		
-		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",9090).usePlaintext().build();
-		
-		// stubs - generate from proto
-		
-		userBlockingStub userStub = userGrpc.newBlockingStub(channel);
-		
-		LoginRequest loginrequest = LoginRequest.newBuilder().setUsername("OLA").setPassword("OLA").build();
-		
-		APIResponse response = userStub.login(loginrequest);
-		
-		System.out.println(response.getResponsemessage());
-		
-		
-	}
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090).usePlaintext().build();
+        userBlockingStub userStub = userGrpc.newBlockingStub(channel);
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        LoginRequest loginRequest = LoginRequest.newBuilder()
+            .setUsername(username)
+            .setPassword(password)
+            .build();
+
+        APIResponse response = userStub.login(loginRequest);
+
+        System.out.println(response.getResponsemessage());
+
+        // Cerrar el canal y el scanner después de su uso
+        channel.shutdown();
+        scanner.close();
+    }
 }
